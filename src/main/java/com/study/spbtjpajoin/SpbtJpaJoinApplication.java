5f6e9c2b8d56cf4f521dbe9ad8ab42205ac1bc85
O-sulloc <bujjaf@gmail.com>
2022-12-01 17:08:57 +0900

    git init

---
+++ src/main/java/com/study/spbtjpajoin/SpbtJpaJoinApplication.java
@@ -0,0 +1,13 @@
+package com.study.spbtjpajoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtJpaJoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbtJpaJoinApplication.class, args);
+    }
+
+}
